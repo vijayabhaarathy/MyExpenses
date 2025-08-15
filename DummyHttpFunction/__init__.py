d8773d9d672235699dd3d10f5c5c74979b55bf37
vijay <56192639+vijayabhaarathy@users.noreply.github.com>
2025-08-15 13:39:51 +0530

    Delete DummyHttpFunction directory

--- DummyHttpFunction/__init__.py
+++
@@ -1,6 +0,0 @@
-import azure.functions as func
-import logging
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("🚀 Dummy HTTP function was called.")
-    return func.HttpResponse("Hello from Dummy Function!", status_code=200)
